8c911958883a6df66ddf0219bc85cb19bf29e522
gleiMoura <gle.bar.mou@gmail.com>
2025-09-16 00:10:57 -0300

    ♻️ refactor: change gitignore and main

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 #Carregar rede de txt
 G=nx.read_edgelist('citation.edgelist.txt')
 
+#informações básicas sobre a rede
 print("Número de Nós:", G.number_of_nodes()) #Calcula número de nós
 print("Número de Arestas:", G.number_of_edges()) #Calcula número de arestas
 print("Direcionada?", G.is_directed())#Verifica se é direcionada
